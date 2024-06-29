e8469432b48ae92a13eee41fc01331fac5436d0b
GAEAlimited <69316708+GAEAlimited@users.noreply.github.com>
2024-06-29 06:19:01 -0400

    Create hello.js

---
+++ pages/api/hello.js
@@ -0,0 +1,12 @@
+import { Redis } from '@upstash/redis'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const redis = new Redis({
+  url: 'https://ideal-toucan-49306.upstash.io',
+  token: 'AcCaAAIncDE2MTMwYWE4ODRmZTY0ZjNkODNiOTg3MjUzMzdhYWE1OXAxNDkzMDY',
+})
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  let count = await redis.incr("counter")
+  res.status(200).json({count: count})
+}
